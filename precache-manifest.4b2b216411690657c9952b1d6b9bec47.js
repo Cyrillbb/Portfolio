2ce0dadd736a4a463499087e9c2dd8755c8ceee3
Fallopio <kibaryshnikov@yandex.ru>
2020-07-22 20:57:01 +0400

    Updates

---
+++ precache-manifest.4b2b216411690657c9952b1d6b9bec47.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df742e3a7740d974a8f916f7938e068a",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "0209cc9515af7aa2f710",
+    "url": "/Portfolio/static/css/main.d29a1c01.chunk.css"
+  },
+  {
+    "revision": "641ad4fca561fc969321",
+    "url": "/Portfolio/static/js/2.41543c8e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio/static/js/2.41543c8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0209cc9515af7aa2f710",
+    "url": "/Portfolio/static/js/main.09ea5a77.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "64a81f8973a0ab0d26797db7e0267ecd",
+    "url": "/Portfolio/static/media/Searcher.64a81f89.png"
+  },
+  {
+    "revision": "2879e7128f70ca42ae4ca2d0ecbfce0e",
+    "url": "/Portfolio/static/media/SpaceStuff.2879e712.png"
+  },
+  {
+    "revision": "d982dcd1b53897972973b1f30df9d5cb",
+    "url": "/Portfolio/static/media/background.d982dcd1.jpg"
+  },
+  {
+    "revision": "cc74d443dbb146e67500967511ce1741",
+    "url": "/Portfolio/static/media/chat.cc74d443.png"
+  }
+]);
